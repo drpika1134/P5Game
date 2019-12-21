462e69d3086b9e2b5ac9cd8d7a9041ec9d53b159
U-DESKTOP-VEMDLHR\PhilippQuach <quach.philipp@googlemail.com>
2019-12-21 20:26:50 +0100

    mapshit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,193 @@
+let tile;
+let grid;
+let w = 40;
+let lastPosX = null;
+let lastPosY = null;
+let prevTile;
+
+// CAMERA MOVEMENT
+let camx = 0;
+let camy = 0;
+let offsetX = 0;
+let offsetY = 0;
+
+let x = 0;
+let y = 0;
+
+let dragged = false;
+let dragging = false;
+
+// wheel zoom
+let zoomWidth = w;
+let zoomHeight = w;
+let mouseScroll = false;
+
+const cols = 60;
+const rows = 60;
+p5.disableFriendlyErrors = true;
+
+function make2DArray(cols, rows) {
+  let array = new Array(cols);
+  for (let i = 0; i < cols; i++) {
+    array[i] = new Array(rows);
+  }
+  return array;
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+
+
+  grid = make2DArray(cols, rows);
+
+  for (let i = 0; i < cols; i++) {
+    for (let x = 0; x < rows; x++) {
+      grid[i][x] = new Tile(i * w, x * w, w, 'green')
+    }
+  }
+  noLoop();
 }
-// test comment 123
+
 function draw() {
-	background(220);
-	rect(10, 10, 10, 10)
-	rect(20, 20, 50, 50)
-	rect(20, 0, 300, 10)
+  background(220);
+
+  // Only translate the map if it's not being dragged
+  if (!dragged) {
+    translate(camx, camy);
+  }
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const square = grid[i][j];
+      // The higher the multiplier, the more cluster it gets
+      const r = noise(i * 0.3, j * 0.3);
+      if (!dragged)
+        square.initialize(offsetX, offsetY, 0, 0, false, r);
+      else
+        square.initialize(0, 0, x, y, true, r);
+    }
+  }
 }
-//hi
-function windowResized() {
-	// commit
-	resizeCanvas(windowWidth, windowHeight)
+
+function mousePressed() {
+  if (mouseButton === RIGHT) {
+    offsetX = mouseX - x;
+    offsetY = mouseY - y;
+    dragging = true;
+  } else {
+    lastPosX = null;
+    lastPosY = null;
+
+    // Get pos in array
+    const xPosInArray = Math.floor((mouseX - x) / w);
+    const yPosInArray = Math.floor((mouseY - y) / w);
+
+    // Just hard-coding the value 40 (the number columns and rows)
+    if (xPosInArray > cols || xPosInArray < 0 || yPosInArray > rows || yPosInArray < 0) {
+      return;
+    }
+    const tile = grid[xPosInArray][yPosInArray]
+
+    // if (tile !== undefined) {
+    // change status to clicked and color to pink
+    tile.clicked = !tile.clicked;
+    tile.color = "pink";
+    tile.initialize(0, 0, x, y, true);
+    // }
+
+  }
 }
-// another test
-// my name is jeff
-// lol
-// hieu gae
+
+function mouseDragged() {
+  if (dragging) {
+    camx = mouseX;
+    camy = mouseY;
+    dragged = false;
+    redraw();
+  }
+}
+
+function mouseReleased() {
+  if (dragging) {
+    // Quit dragging
+    dragging = false;
+
+    x = mouseX - offsetX;
+    y = mouseY - offsetY;
+
+    dragged = true;
+
+    redraw();
+  }
+}
+
+/*function mouseMoved() {
+  // O(1) solution for hovering and clicking tiles
+
+  // Get the index of the tile hovered in array
+  const xPosInArray = Math.floor((mouseX - x) / w);
+  const yPosInArray = Math.floor((mouseY - y) / w);
+  // Just hard-coding the value 40 (the number columns and rows)
+  if (xPosInArray > 40 || xPosInArray < 0 || yPosInArray > 40 || yPosInArray < 0) {
+    return;
+  }
+  const tile = grid[xPosInArray][yPosInArray];
+
+  // Check if the mouse had been inside before
+  if (lastPosX !== null && lastPosY !== null) {
+    prevTile = grid[lastPosX][lastPosY];
+
+    // Only reset if the tile is not clicked
+    if (!prevTile.clicked) {
+      // Reset the last element to its normal state
+      prevTile.color = "green";
+      prevTile.draw(0, 0, x, y, true);
+    }
+  }
+  if (!tile.clicked) {
+    // Change color to red when hovered over
+    tile.color = "red";
+    tile.draw(0, 0, x, y, true);
+  }
+  // Check if the last element is the same as the element being hovered on
+  // If not, set it to the currnt element
+  lastPosX = lastPosX !== xPosInArray ? xPosInArray : lastPosX;
+  lastPosY = lastPosY !== yPosInArray ? yPosInArray : lastPosY;
+}
+*/
+let zoomLevel = 1;
+// Zooming
+// function mouseWheel(event) {
+//   if (event.delta < 0) {
+//     zoomLevel += 0.2
+//   } else {
+//     if (zoomLevel > 1) {
+//       zoomLevel -= 0.2    
+//     }
+//     if (zoomLevel === 0) zoomLevel = 1;
+//   }
+
+// //   if (event.delta < 0) {
+// //     if (!(zoomWidth >= 130 || zoomHeight >= 130)) {
+// //       zoomWidth += event.delta/10 + w;
+// //       zoomHeight += event.delta/10 + w;
+// //     }
+// //   } else {
+// //     if ((zoomWidth >= 0 || zoomHeight >= 0)) {
+// //       zoomWidth -= event.delta/10 + w;
+// //       zoomHeight -= event.delta/10 + w;
+// //     }
+// //   }
+
+// //   if (zoomWidth <= 0 || zoomHeight <= 0) {
+// //     zoomWidth = w;
+// //     zoomHeight = w;
+// //   }
+
+// //   // console.log(zoomWidth, zoomHeight);
+// //   for (let i = 0; i < cols; i++) {
+// //     for (let x = 0; x < rows; x++) {
+// //       grid[i][x] = new Tile(i * zoomWidth, x * zoomHeight, zoomWidth, 'green')
+// //     }
+// //   }
+//   redraw();
+// }
