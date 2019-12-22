fb1c421e13355e9643088b9c4009a93460a865d9
drpika1134 <drpika1134@gmail.com>
2019-12-22 15:08:48 -0600

    nodejs environment

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -26,15 +26,7 @@ p5.disableFriendlyErrors = true
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
-  grid = make2DArray(cols, rows)
-
-  // Initialize Tile objects
-  for (let i = 0; i < cols; i++) {
-    for (let x = 0; x < rows; x++) {
-      // From top to bottom
-      grid[i][x] = new Tile(i * tileWidth, x * tileWidth, tileWidth, 'green')
-    }
-  }
+  initializeGrid()
   noLoop()
 }
 
@@ -45,29 +37,15 @@ function draw() {
   if (!dropped) {
     translate(camx, camy)
   }
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      const square = grid[i][j]
-      // The higher the multiplier, the more cluster it gets
-      const r = noise(i * 0.3, j * 0.3)
 
-      // Is the map dropped or still being dragged
-      if (!dropped) {
-        square.initialize(offsetX, offsetY, 0, 0, false, r)
-      } else {
-        square.initialize(0, 0, x, y, true, r)
-      }
-    }
-  }
+  drawTiles()
 }
 
 function mousePressed() {
   // Dragging mode if right mouse button is clicked
   // else click on the tile
   if (mouseButton === RIGHT) {
-    offsetX = mouseX - x
-    offsetY = mouseY - y
-    dragging = true
+    startCameraMove()
   } else {
     lastPosX = null
     lastPosY = null
@@ -95,24 +73,14 @@ function mousePressed() {
 
 function mouseDragged() {
   if (dragging) {
-    camx = mouseX
-    camy = mouseY
-    dropped = false
+    setCameraPosition()
     redraw()
   }
 }
 
 function mouseReleased() {
   if (dragging) {
-    // Quit dragging
-    dragging = false
-
-    // Set new origin
-    x = mouseX - offsetX
-    y = mouseY - offsetY
-
-    dropped = true
-
+    quitCameraMode()
     redraw()
   }
 }
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-function make2DArray(cols, rows) {
-  let array = new Array(cols)
-  for (let i = 0; i < cols; i++) {
-    array[i] = new Array(rows)
-  }
-  return array
-}
