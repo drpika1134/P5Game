f3a687043ea1ff1d77196046f1fa9d5d007a2b7d
drpika1134 <drpika1134@gmail.com>
2019-12-22 16:16:42 -0600

    terrain gen improvements

--- src/client/js/tile.js
+++ src/client/js/tile.js
@@ -7,32 +7,26 @@ class Tile {
     this.clicked = false
   }
   initialize(offsetX, offsetY, newOriginX, newOriginY, dragging, r) {
-    //0.4, 0.5, 0.7, 1
     if (dragging) {
-      if (r <= 0.35) {
-        this.color = 'blue'
-      } else if (r <= 0.6) {
-        this.color = 'green'
-      } else if (r <= 0.7) {
-        this.color = '#26660f'
-      } else if (r <= 1) {
-        this.color = '#ad4315'
-      }
+      this.generateTerrain(r)
       fill(this.color)
       // Update the new position after dragging
       rect(this.x + newOriginX, this.y + newOriginY, this.w, this.w)
     } else {
-      if (r <= 0.35) {
-        this.color = 'blue'
-      } else if (r <= 0.6) {
-        this.color = 'green'
-      } else if (r <= 0.7) {
-        this.color = '#26660f'
-      } else if (r <= 1) {
-        this.color = '#ad4315'
-      }
+      this.generateTerrain(r)
       fill(this.color)
       rect(this.x - offsetX, this.y - offsetY, this.w, this.w)
+    }
+  }
+  generateTerrain(r) {
+    if (r <= 0.35) {
+      this.color = 'blue'
+    } else if (r <= 0.52 && r >= 0.47) {
+      this.color = '#26660f' // forests
+    } else if (r <= 0.65) {
+      this.color = 'green'
+    } else if (r <= 0.97) {
+      this.color = '#ad4315' // mountains
     }
   }
 }
