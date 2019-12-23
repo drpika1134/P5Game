51abc1417909f831b69d37d9a2a91f5a9007a88d
drpika1134 <drpika1134@gmail.com>
2019-12-23 16:22:54 -0600

    random spawn location

--- src/client/js/map.js
+++ src/client/js/map.js
@@ -9,7 +9,7 @@ function initializeGrid() {
     }
   }
 }
-
+let flag = false
 function drawTiles() {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
@@ -20,16 +20,33 @@ function drawTiles() {
       // Is the map dropped or still being dragged
       if (!dropped) {
         // TODO: player location need to be random but close to teamates
-        if (i == 1 && j == 1) {
-          square.tileInfo.playerBase = {
-            name: 'DrPika',
-            id: player.id
-          }
-        }
         square.initialize(offsetX, offsetY, 0, 0, false, r)
       } else {
         square.initialize(0, 0, x, y, true, r)
       }
     }
   }
+  if (flag == false) {
+    let random =
+      possibleSpawnLocation[getRandomInt(5, possibleSpawnLocation.length / 2)]
+    const square = grid[random[0]][random[1]]
+    square.tileInfo.playerBase = {
+      name: 'DrPika',
+      id: player.id
+    }
+    square.initialize(
+      offsetX,
+      offsetY,
+      0,
+      0,
+      false,
+      noise(random[0] * 0.3, random[1] * 0.3)
+    )
+    flag = true
+  }
+}
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
 }
