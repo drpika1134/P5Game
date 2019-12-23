9f31cae0c764039d264dc2d3d5a2c48cbcc2c16a
drpika1134 <drpika1134@gmail.com>
2019-12-23 16:33:33 -0600

    camera improvement

---
+++ src/client/js/camera.js
@@ -0,0 +1,28 @@
+window.onload = addListeners()
+var offX
+var offY
+
+function addListeners() {
+  document
+    .getElementById('inner')
+    .addEventListener('mousedown', mouseDown, false)
+  window.addEventListener('mouseup', mouseUp, false)
+}
+
+function mouseUp() {
+  window.removeEventListener('mousemove', divMove, true)
+}
+
+function mouseDown(e) {
+  var div = document.getElementById('inner')
+  offY = e.clientY - parseInt(div.offsetTop)
+  offX = e.clientX - parseInt(div.offsetLeft)
+  window.addEventListener('mousemove', divMove, true)
+}
+
+function divMove(e) {
+  var div = document.getElementById('inner')
+  div.style.position = 'absolute'
+  div.style.top = e.clientY - offY + 'px'
+  div.style.left = e.clientX - offX + 'px'
+}
