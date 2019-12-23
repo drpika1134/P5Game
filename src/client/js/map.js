5f792f8d2f83cb33082eabb69dd95631ec1a5586
drpika1134 <drpika1134@gmail.com>
2019-12-23 17:08:52 -0600

    cleanups

--- src/client/js/map.js
+++ src/client/js/map.js
@@ -9,7 +9,7 @@ function initializeGrid() {
     }
   }
 }
-let flag = false
+
 function drawTiles() {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
@@ -17,36 +17,22 @@ function drawTiles() {
       // The higher the multiplier, the more cluster it gets
       const r = noise(i * 0.3, j * 0.3)
 
-      // Is the map dropped or still being dragged
-      if (!dropped) {
-        // TODO: player location need to be random but close to teamates
-        square.initialize(offsetX, offsetY, 0, 0, false, r)
-      } else {
-        square.initialize(0, 0, x, y, true, r)
-      }
+      square.initialize(r)
     }
   }
-  if (flag == false) {
-    let random =
-      possibleSpawnLocation[getRandomInt(5, possibleSpawnLocation.length / 2)]
-    const square = grid[random[0]][random[1]]
-    square.tileInfo.playerBase = {
-      name: 'DrPika',
-      id: player.id
-    }
-    square.initialize(
-      offsetX,
-      offsetY,
-      0,
-      0,
-      false,
-      noise(random[0] * 0.3, random[1] * 0.3)
-    )
-    flag = true
+  // TODO: Teammate should spawn closer to each other
+  if (isPlayerSpawned == false) {
+    spawn()
   }
 }
-function getRandomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
+function spawn() {
+  let random =
+    possibleSpawnLocation[getRandomInt(5, possibleSpawnLocation.length / 2)]
+  const square = grid[random[0]][random[1]]
+  square.tileInfo.playerBase = {
+    name: 'DrPika',
+    id: player.id
+  }
+  square.initialize(noise(random[0] * 0.3, random[1] * 0.3))
+  isPlayerSpawned = true
 }
